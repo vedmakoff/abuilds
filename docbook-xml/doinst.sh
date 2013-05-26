8e18d31eb74390aa6736e5b863de15fac8bac04f
Sir Anthony <anthony@adsorbtion.org>
2013-05-26 22:09:55 +0600

    Core rebuilded.

--- docbook-xml/doinst.sh
+++ docbook-xml/doinst.sh
@@ -3,19 +3,19 @@ if [ -e etc/xml/catalog.preserve ]; then
 elif [ ! -e etc/xml/catalog ]; then
 	xmlcatalog --noout --create etc/xml/catalog
 fi
-xmlcatalog --noout --add "delegatePublic" \
+xmlcatalog --create --noout --add "delegatePublic" \
 	"-//OASIS//ENTITIES DocBook XML" \
 	"file:///etc/xml/docbook-xml" \
 	etc/xml/catalog
-xmlcatalog --noout --add "delegatePublic" \
+xmlcatalog --create --noout --add "delegatePublic" \
 	"-//OASIS//DTD DocBook XML" \
 	"file:///etc/xml/docbook-xml" \
 	etc/xml/catalog
-xmlcatalog --noout --add "delegateSystem" \
+xmlcatalog --create --noout --add "delegateSystem" \
 	"http://www.oasis-open.org/docbook/" \
 	"file:///etc/xml/docbook-xml" \
 	etc/xml/catalog
-xmlcatalog --noout --add "delegateURI" \
+xmlcatalog --create --noout --add "delegateURI" \
 	"http://www.oasis-open.org/docbook/" \
 	"file:///etc/xml/docbook-xml" \
 	etc/xml/catalog
