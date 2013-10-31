0eaa6fd3030233c9320931c9a53f398567ff39ca
SirAnthony <anthony@adsorbtion.org>
2013-10-31 12:41:43 +0100

    Build deps update

--- check_bdeps.sh
+++ check_bdeps.sh
@@ -2,6 +2,12 @@
 ABUILDS_FILE=noabuilds.html
 ERROR_FILE=errorabuilds.html
 OUT_FILES="${ABUILDS_FILE} ${ERROR_FILE}"
+ABUILDS_FOLDER=${1:-.}/
+if [ ! -z ${BUILD_LIST} ]; then
+    ABUILD_PATHS=`cat ${BUILD_LIST} | sed "/#/d; /^\s*$/d; s|^|$ABUILDS_FOLDER|"`
+else
+    ABUILD_PATHS=${ABUILDS_FOLDER}*
+fi
 
 for filename in ${OUT_FILES}; do
     cat >$filename << END
@@ -10,20 +16,18 @@ for filename in ${OUT_FILES}; do
     <title>Packages without abuilds</title>
 </head>
 <body>
+<h3>Total: </h3>
 END
 done
 
-for path in *; do
+for path in ${ABUILD_PATHS}; do
     if [ ! -d $path ]; then continue; fi
     if [ -f $path/ABUILD ]; then
-        (   errors=""
+        (
             . $path/ABUILD 2>>${ERROR_FILE}
-            if [[ ! -z $errors ]]; then
-                echo "<p>${errors}</p>"
-            fi
             if [[ -z "${build_deps}" ]]; then
                 bname=$(basename $path)
-                echo "<p>${bname}</p>" >> ${ABUILDS_FILE}
+                echo "${bname}<br/>" >> ${ABUILDS_FILE}
             fi
         )
     fi
@@ -32,3 +36,10 @@ done
 for filename in ${OUT_FILES}; do
     echo "</body></html>" >> $filename
 done
+
+for filename in ${OUT_FILES}; do
+    COUNT=$(cat ${filename} | sed '/^\s*</d' | wc -l)
+    sed -i "s/\(Total: \)[^<]*/\1${COUNT}/" ${filename}
+done
+
+sed -i '/^\s*</! s|$|<br/>|g' ${ERROR_FILE}
